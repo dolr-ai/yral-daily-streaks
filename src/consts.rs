2d3fe3e7b9b3b11fd11d77e71f41289bd88f25f8
naitik-makwana-yral <naitik@gobazzinga.io>
2026-04-10 02:02:57 +0530

    fix: auth provider input

--- src/consts.rs
+++ src/consts.rs
@@ -8,5 +8,4 @@ pub static CLAIMS: Lazy<Claims> = Lazy::new(|| Claims {
     exp: 317125598072, // TODO: To be changed later when expiring tokens periodically
 });
 
-pub const YRAL_AUTH_V2_ACCESS_TOKEN_ISS_1: &str = "https://auth.yral.com";
-pub const YRAL_AUTH_V2_ACCESS_TOKEN_ISS_2: &str = "https://auth.dolr.ai";
+pub const EXPECTED_AUTH_ISSUERS: &[&str] = &["https://auth.yral.com", "https://auth.dolr.ai"];
